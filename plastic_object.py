20670f6a017ae3cb3e4434e1578e7e774d30af91
cvanness197 <166647418+CarsonV8824@users.noreply.github.com>
2025-11-15 11:00:19 -0600

    Update plastic_object.py

--- plastic_object.py
+++ plastic_object.py
@@ -1,7 +1,16 @@
 import pygame
 pygame.init()
 
+info = pygame.display.Info()
+info = pygame.display.Info()
+
+
+
+SCREEN_WIDTH = info.current_w
+SCREEN_HEIGHT = info.current_h - 85
+
+
 class Plastic(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("")
+        self.image = pygame.image.load("Images/Plastic_Bag.png")
