4db5fa8447935a029f84d4a730b96dfb113815ea
Titanium19862027 <166647767+iamnotthatguydude@users.noreply.github.com>
2025-11-15 10:45:33 -0600

    Done

---
+++ turtle_object.py
@@ -0,0 +1,25 @@
+<<<<<<< Updated upstream:turtle.py
+class Turtle_object:
+=======
+import pygame
+>>>>>>> Stashed changes:turtle_object.py
+
+class Turtle_object(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.lives = 3
+        self.score = 0
+    
+    def lose_life(self):
+        if self.lives > 0:
+            self.lives -= 1
+        elif self.lives == 0:
+            return True
+        return False
+    
+    def gain_score(self, points):
+        self.score += points
