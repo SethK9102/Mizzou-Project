f05d9d70daa8b3e39b1fe13176f211b5e48bfe57
cvanness197 <166647418+CarsonV8824@users.noreply.github.com>
2025-11-15 10:35:54 -0600

    s

---
+++ sound.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.mixer.init()
+
+class Sound():
+    def __init__(self, sound_file:str="", volume:float=0.5):
+        self.sound  = pygame.mixer.Sound(sound_file)
+        self.sound.set_volume(volume)
+    def play(self):
+        self.sound.play()
+    def stop(self):
+        self.sound.stop()
+        
+pygame.quit()
