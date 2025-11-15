bbb2ca6c5780e3a8cdf991c6d99194b7415fdd49
Titanium19862027 <166647767+iamnotthatguydude@users.noreply.github.com>
2025-11-15 10:27:26 -0600

    turtle_object created and restart logic began

---
+++ main.py
@@ -0,0 +1,6 @@
+from turtle_object import Turtle_object
+
+trtl = Turtle_object()
+
+    trtl.lose_life(running)
+    running = False
--- turtle_object.py
+++ turtle_object.py
@@ -1,15 +1,15 @@
-class Turtle:
+class Turtle_object:
 
     def __init__(self):
         self.lives = 3
         self.score = 0
     
-    def lose_life(self):
+    def lose_life(self, running):
         if self.lives > 0:
             self.lives -= 1
         elif self.lives == 0:
-            return True
-        return False
-    
+            running = False
+
     def gain_score(self, points):
         self.score += points
+        return self.score
